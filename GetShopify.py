7623f41027ad24d0aaeeda5a1e3b4d0dd269ae4e
jed <jed.reisner@gmail.com>
2018-11-08 23:44:09 -0700

    Added logic to update existing users if they don't have a first and last name in mailchimp.

--- GetShopify.py
+++ GetShopify.py
@@ -12,7 +12,7 @@ date = datetime.now() - timedelta(days=1)
 #Sets URL and parameters for request
 url = Properties.shopifyURL
 params = dict(
-    created_at_min = date,
+    #created_at_min = date,
     fields='name,contact_email,buyer_accepts_marketing,note_attributes,billing_address'
 )
 
@@ -27,6 +27,7 @@ logging.debug(r.status_code)
 logging.debug(r.reason)
 logging.debug(r.content)
 if r.status_code != 200:
+    logging.error("Shopify request failed:")
     logging.error(r.status_code)
     logging.error(r.reason)
     logging.error(r.content)
@@ -37,6 +38,7 @@ mailchimpBaseURL = Properties.mailchimpBaseURL
 
 #Iterates through orders and assigns variables to be used in POST/UPDATE
 for order in orders:
+    # orderNumber = order['order_number']
     acceptsMarketing = order['buyer_accepts_marketing']
     email = order['contact_email']
     firstName = order['billing_address']['first_name']
@@ -46,6 +48,8 @@ for order in orders:
     for note in order['note_attributes']:
         if note['name'] == 'Club':
             club = note['value']
+            if not club:
+                logging.error(("No clubID found in order: "+orderNumber))
 
     #Verifies user accepts marketing then attempts to add user to Mailchimp list as new subscriber
     if acceptsMarketing:
@@ -56,23 +60,35 @@ for order in orders:
         logging.debug(postSub.reason)
         logging.debug(postSub.content)
         if postSub.status_code != 200:
+            logging.error("User could not be added:")
             logging.error(postSub.status_code)
             logging.error(postSub.reason)
             logging.error(postSub.content)
 
 
         #If attempt to add new user fails, attempts to update user by doing a lookup.
-        #TODO Get /search/members for member and then patch member with /lists/9e67587f52/members/SUBSCRIBER_ID
-        # if postSub.status_code != "200":
-        #     updateSub = requests.put((mailchimpBaseURL + "/lists/" + club + "/members"), data=subscriber, auth=('python', mailchimpAPIKey))
-        #     logging.debug(updateSub.status_code)
-        #     logging.debug(updateSub.reason)
-        #     logging.debug(updateSub.content)
-        #     if updateSub.status_code != 200:
-        #         logging.error(updateSub.status_code)
-        #         logging.error(updateSub.reason)
-        #         logging.error(updateSub.content)
-        #
-        #     print(addSub.status_code, addSub.reason)
-        #     print(addSub.content)
-        #     print(addSub.reason)
+        if postSub.status_code != "200":
+            searchSub = requests.get((Properties.mailchimpSearchURL + email), auth=('python', mailchimpAPIKey))
+            logging.debug(searchSub.status_code)
+            logging.debug(searchSub.reason)
+            logging.debug(searchSub.content)
+            matchData = searchSub.json()
+            members = matchData['exact_matches']['members']
+            #Iterates through exact matches for the email address
+            for member in members:
+                listId = member['list_id']
+                userId = member['id']
+                mergeFirstName = member['merge_fields']['FNAME']
+                #Checks for null firstname field and updates user if it isn't present
+                if not mergeFirstName:
+                    updateSub = requests.put((mailchimpBaseURL+"/lists/"+listId+"/members/"+userId), data=subscriber, auth=('python', mailchimpAPIKey))
+                    logging.debug(updateSub.status_code)
+                    logging.debug(updateSub.reason)
+                    logging.debug(updateSub.content)
+
+                    if updateSub.status_code != 200:
+                        logging.error("User could not be updated:")
+                        logging.error(updateSub.status_code)
+                        logging.error(updateSub.reason)
+                        logging.error(updateSub.content)
+
